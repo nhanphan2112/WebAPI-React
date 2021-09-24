d7af11579a1e5755a7d30ce5bed07f6c5b7adfc3
Nhan Phan <nphanacc@gmail.com>
2021-09-24 09:59:15 -0700

    complete navigation layout and install react router dom to work with navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Home from "./Home";
+import Department from "./Department";
+import Employee from "./Employee";
+import Navigation from "./Navigation";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-     <h3 className="m-3 d-flex justify-content-center">React JS Tutorial</h3>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<div className="container">
+				<h3 className="m-3 d-flex justify-content-center">React JS Tutorial</h3>
+				<Navigation />
+
+				<Switch>
+					<Route path="/" component={Home} exact />
+					<Route path="/department" component={Department} exact />
+					<Route path="/employee" component={Employee} exact />
+				</Switch>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/Department.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Department extends Component {
+	render() {
+		return <div className="mt-5 d-flex justify-content-left">This is Department page</div>;
+	}
+}
+
+export default Department;
